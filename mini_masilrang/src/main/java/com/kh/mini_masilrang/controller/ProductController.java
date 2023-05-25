5c483e3ea72713f5da3abdd6d9fc8d3246cafc21
woojin <sun65568@hanmail.net>
2023-05-25 15:43:16 +0900

    관심 상품 기능 추가

--- mini_masilrang/src/main/java/com/kh/mini_masilrang/controller/ProductController.java
+++ mini_masilrang/src/main/java/com/kh/mini_masilrang/controller/ProductController.java
@@ -1,12 +1,15 @@
 package com.kh.mini_masilrang.controller;
 
+import com.kh.mini_masilrang.dao.MemberDAO;
 import com.kh.mini_masilrang.dao.ProductDAO;
+import com.kh.mini_masilrang.vo.LikeProductVO;
 import com.kh.mini_masilrang.vo.ProductVO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
@@ -29,6 +32,21 @@ public class ProductController {
     public ResponseEntity<List<ProductVO>> ProductInfo(@RequestParam int product) {
         ProductDAO dao = new ProductDAO();
         List<ProductVO> list = dao.ProductInfo(product);
+        return new ResponseEntity<>(list, HttpStatus.OK);
+    }
+
+    @PostMapping("/insertLikeProduct")
+    public ResponseEntity<Boolean> insertLikeProduct(@RequestBody Map<String, Integer> insertProduct) {
+        int userNo = insertProduct.get("userNum");
+        int productNo = insertProduct.get("productNum");
+        ProductDAO dao = new ProductDAO();
+        boolean isTrue =dao.insertLikeProduct(productNo,userNo);
+        return new ResponseEntity<>(isTrue, HttpStatus.OK);
+    }
+    @GetMapping("/likeProduct")
+    public ResponseEntity<List<LikeProductVO>> likeProductInfo(@RequestParam int userNo) {
+        ProductDAO dao = new ProductDAO();
+        List<LikeProductVO> list = dao.likeProduct(userNo);
         return new ResponseEntity<>(list, HttpStatus.OK);
     }
 }
--- mini_masilrang/src/main/java/com/kh/mini_masilrang/dao/ProductDAO.java
+++ mini_masilrang/src/main/java/com/kh/mini_masilrang/dao/ProductDAO.java
@@ -1,6 +1,7 @@
 package com.kh.mini_masilrang.dao;
 
 import com.kh.mini_masilrang.common.Common;
+import com.kh.mini_masilrang.vo.LikeProductVO;
 import com.kh.mini_masilrang.vo.ProductVO;
 
 import java.sql.Connection;
@@ -8,6 +9,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class ProductDAO {
@@ -26,7 +28,7 @@ public class ProductDAO {
             pStmt = conn.prepareStatement(sql);
             pStmt.setString(1, checked);
             rs = pStmt.executeQuery();
-            while(rs.next()) {
+            while (rs.next()) {
                 ProductVO vo = new ProductVO();
                 int product_no = rs.getInt("PRODUCT_NO");
                 String product_name = rs.getString("PRODUCT_NAME");
@@ -45,11 +47,12 @@ public class ProductDAO {
             Common.close(rs);
             Common.close(pStmt);
             Common.close(conn);
-        } catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return list1;
     }
+
     //테마 선택 시 정보 받아오기
     public List<ProductVO> productThemeFind(String theme) {
         List<ProductVO> list2 = new ArrayList<>();
@@ -60,7 +63,7 @@ public class ProductDAO {
             pStmt.setString(1, theme);
             rs = pStmt.executeQuery();
 
-            while(rs.next()) {
+            while (rs.next()) {
                 ProductVO vo = new ProductVO();
                 int product_no = rs.getInt("PRODUCT_NO");
                 String product_name = rs.getString("PRODUCT_NAME");
@@ -77,7 +80,7 @@ public class ProductDAO {
             Common.close(rs);
             Common.close(pStmt);
             Common.close(conn);
-        } catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return list2;
@@ -86,10 +89,10 @@ public class ProductDAO {
     public List<ProductVO> ProductInfo(int product) {
         List<ProductVO> list = new ArrayList<>();
 
-        Connection conn =null;
+        Connection conn = null;
         Statement stmt = null;
         ResultSet rs = null;
-        PreparedStatement pStmt =null;
+        PreparedStatement pStmt = null;
 
         try {
             System.out.print("DB에 접속 중입니다.");
@@ -131,6 +134,62 @@ public class ProductDAO {
             Common.close(stmt);
             Common.close(conn);
 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    // 관심 상품 추가
+    public boolean insertLikeProduct(int userNo, int productNo) {
+        int result = 0;
+        String sql = "INSERT INTO LIKEPRODUCT (USER_NO, USER_NAME, PRODUCT_NO, PRODUCT_NAME, PRODUCT_IMG, CONTENT1, CONTENT2)\n" +
+                "SELECT M.USER_NO, M.USER_NAME, P.PRODUCT_NO, P.PRODUCT_NAME, P.PRODUCT_IMG, P.CONTENT1, P.CONTENT2\n" +
+                "FROM MEMBER_INFO M\n" +
+                "JOIN PRODUCT P ON M.USER_NO = ? AND P.PRODUCT_NO = ?";
+        try {
+            conn = Common.getConnection();
+            pStmt = conn.prepareStatement(sql);
+            pStmt.setInt(1, userNo);
+            pStmt.setInt(2, productNo);
+            result = pStmt.executeUpdate();
+            System.out.println("관심 상품 추가 결과 : " + result);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Common.close(pStmt);
+        Common.close(conn);
+
+        if (result == 1) return true;
+        else return false;
+    }
+    // 관심 상품 조회
+    public List<LikeProductVO> likeProduct(int userNo) {
+        List<LikeProductVO> list = new ArrayList<>();
+        try {
+            conn = Common.getConnection();
+            stmt = conn.createStatement();
+            String sql = "SELECT PRODUCT_NO, PRODUCT_NAME, CONTENT1, CONTENT2, PRODUCT_IMG FROM LIKEPRODUCT WHERE USER_NO = " + "'" + userNo  + "'";
+            rs = stmt.executeQuery(sql);
+
+            while(rs.next()) {
+                LikeProductVO vo = new LikeProductVO();
+                int product_no = rs.getInt("PRODUCT_NO");
+                String product_name = rs.getString("PRODUCT_NAME");
+                String content1 = rs.getString("CONTENT1");
+                String content2 = rs.getString("CONTENT2");
+                String product_img = rs.getString("PRODUCT_IMG");
+                vo.setProduct_no(product_no);
+                vo.setProduct_name(product_name);
+                vo.setContent1(content1);
+                vo.setContent2(content2);
+                vo.setProduct_img(product_img);
+                list.add(vo);
+            }
+            Common.close(rs);
+            Common.close(stmt);
+            Common.close(conn);
         } catch (Exception e) {
             e.printStackTrace();
         }
