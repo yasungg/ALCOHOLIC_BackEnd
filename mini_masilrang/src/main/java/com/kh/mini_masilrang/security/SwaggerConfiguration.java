6ceb01d62bc4e0bafe45e699432ef4412ef20e77
JAEHYUNGKWAK <126457219+JAEHYUNGKWAK@users.noreply.github.com>
2023-05-25 21:02:26 +0900

    스웨거 추가

---
+++ mini_masilrang/src/main/java/com/kh/mini_masilrang/security/SwaggerConfiguration.java
@@ -0,0 +1,39 @@
+package com.kh.mini_masilrang.security;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Slf4j
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration {
+
+    @Bean
+    public Docket api() {
+        log.info("스웨거 api() 함수 호출 !!");
+        Docket build = new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.kh.mini_masilrang"))
+                .paths(PathSelectors.any())
+                .build();
+        return build;
+    }
+
+    private ApiInfo apiInfo() {
+        log.info("스웨거 apiInfo() 함수 호출 !!");
+        return new ApiInfoBuilder()
+                .title("Spring Boot Open API Test with Swagger")
+                .description("설명 부분")
+                .version("1.0.0")
+                .build();
+    }
+}
