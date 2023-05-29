1c95782a7394adb2644a400bf2ac38e468821bd7
woojin <sun65568@hanmail.net>
2023-05-29 16:54:23 +0900

    미니 프로젝트 최종

--- mini_masilrang/src/main/java/com/kh/mini_masilrang/common/Common.java
+++ mini_masilrang/src/main/java/com/kh/mini_masilrang/common/Common.java
@@ -8,8 +8,8 @@ import java.sql.Statement;
 public class Common {
     // 오라클 설정 정보 (JDBC 연결)
     final static String ORACLE_URL = "jdbc:oracle:thin:@localhost:1521:xe";
-    final static String ORACLE_ID = "SCOTT";
-    final static String ORACLE_PW = "TIGER";
+    final static String ORACLE_ID = "scott";
+    final static String ORACLE_PW = "tiger";
     final static String ORACLE_DRV = "oracle.jdbc.driver.OracleDriver";
 
     public static Connection getConnection() {
